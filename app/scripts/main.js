5337d673b1a2f56d494ac0b73785f363f5a6d586
rrayd <nakovaina89@gmail.com>
2017-04-17 20:50:21 +0500

    generation drawing region

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,39 @@
-console.log('Welcome to PixelApp by Dmitry Raikov');
+'use strict';
 
+// pixelapp
 
+function pixelapp () {
+
+	// запрещаем выделение текста
+	document.ondragstart = function() { return false };
+	document.body.onselectstart = function() { return false };
+
+	// определяем переменные
+	var pa_factor = 3;
+
+	// генерируем растровое поле
+	raster(pa_factor);
+};
+
+// функция генерации растрового поля
+function raster (pa_factor) {
+	var pa_block_size,
+		pa_block_counter,
+		pa_drawing = document.getElementById('pa-drawing');
+
+	// определение параметров нового поля
+	pa_block_size = 480 / pa_factor;
+	pa_block_counter = pa_factor * pa_factor;
+
+	// генерация
+	for ( var i = 0; i < pa_block_counter; i++ ) {
+		var pa_dblock = document.createElement('div');
+		pa_dblock.className = 'pa-db';
+		pa_dblock.style.width = pa_block_size + 'px';
+		pa_dblock.style.height = pa_block_size + 'px';
+		pa_drawing.appendChild(pa_dblock);
+	}
+};
+
+// запуск приложения
+pixelapp();
